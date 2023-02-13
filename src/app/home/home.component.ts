fe658c4eeaa0a071129d221b0b0d53e5d1037a50
hannah68 <reihaneh.naderi1989@gmail.com>
2023-02-13 16:45:19 +0000

    create a half home componet functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  trendingMovies: any;
+  constructor(private http: HttpClient){}
 
+  ngOnInit(){
+    this.getTrendingMovies();
+  }
+
+  getTrendingMovies(){
+    this.http.get('http://localhost:4200/assets/data/trending-movies.json').subscribe((movies) => {
+      console.log(movies)
+      this.trendingMovies = movies;
+    })
+  }
 }
